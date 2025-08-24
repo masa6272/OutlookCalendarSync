f7ad32ffbb4e22b7421756df11536d596ec3e4fe
masa6272 <70428507+masa6272@users.noreply.github.com>
2025-08-25 01:59:41 +0900

    各時間帯で最大1つに予定をまとめる
    - 予定が重なっている時間帯は、最も優先度の高いBusyStatusを選択

--- export.py
+++ export.py
@@ -12,7 +12,7 @@ SECRET_TOKEN = os.getenv("SECRET_TOKEN")
 ICS_FILE = os.getenv("ICS_FILE")
 
 busy_status = {
-    0: "予定なし",  # olFree
+    0: "空き時間",  # olFree
     1: "仮の予定",  # olTentative
     2: "予定あり",  # olBusy
     3: "不在",  # olOutOfOffice
@@ -38,24 +38,30 @@ def send_calendar():
     restricted_items = items.Restrict(restriction)
 
     # --- JSON 作成 ---
-    events = []
+    events_allday = []
+    events_timed = []
     for item in restricted_items:
         tz = datetime.timezone(item.Start - item.StartUTC)
         if item.AllDayEvent:
-            ev = {
-                "summary": (item.Subject),
-                "start": item.Start.replace(tzinfo=tz).strftime("%Y-%m-%d"),
-                "end": item.End.replace(tzinfo=tz).strftime("%Y-%m-%d"),
-                "isAllDay": item.AllDayEvent,
-            }
+            events_allday.append(
+                {
+                    "summary": item.Subject,
+                    "start": item.Start.replace(tzinfo=tz).strftime("%Y-%m-%d"),
+                    "end": item.End.replace(tzinfo=tz).strftime("%Y-%m-%d"),
+                    "isAllDay": item.AllDayEvent,
+                }
+            )
         else:
-            ev = {
-                "summary": busy_status[int(item.BusyStatus)],
-                "start": item.Start.replace(tzinfo=tz).strftime("%Y-%m-%dT%H:%M:%S%z"),
-                "end": item.End.replace(tzinfo=tz).strftime("%Y-%m-%dT%H:%M:%S%z"),
-                "isAllDay": item.AllDayEvent,
-            }
-        events.append(ev)
+            events_timed.append(
+                {
+                    # "summary": item.Subject,
+                    "start": item.Start.replace(tzinfo=tz).strftime("%Y-%m-%dT%H:%M:%S%z"),
+                    "end": item.End.replace(tzinfo=tz).strftime("%Y-%m-%dT%H:%M:%S%z"),
+                    # "isAllDay": item.AllDayEvent,
+                    "status": int(item.BusyStatus),
+                }
+            )
+    events = events_allday + merge_events(events_timed)
     print(events)
 
     # ====== POST リクエスト送信 ======
@@ -65,6 +71,172 @@ def send_calendar():
         json=events,  # 本文に JSON を送信
     )
     print(response.status_code, response.text)
+
+
+def merge_events(events):
+    """時間帯が重なっている予定と連続している予定をマージする"""
+    if not events:
+        return []
+
+    print(f"original: {events}")
+    # 区切りとなる時刻を抽出
+    timestamps_boundary = []
+    for event in events:
+        start = datetime.datetime.fromisoformat(event["start"])
+        end = datetime.datetime.fromisoformat(event["end"])
+        status = event["status"]
+        timestamps_boundary.append({"timestamp": start, "status": status})
+        timestamps_boundary.append({"timestamp": end, "status": -1})
+    # 時刻でソート
+    timestamps_boundary.sort(key=lambda x: x["timestamp"])
+
+    print(f"boundary: {timestamps_boundary}")
+
+    # boundaryで区切って、全部の時間帯を抽出
+    timerange_status_all = []
+    for event in events:
+        start = datetime.datetime.fromisoformat(event["start"])
+        end = datetime.datetime.fromisoformat(event["end"])
+        status = event["status"]
+
+        for boundary in timestamps_boundary:
+            boundary_ts = boundary["timestamp"]
+            if start < boundary_ts < end:
+                # start - boundary, boundary - end に分割
+                timerange_status_all.append({"start": start, "end": boundary_ts, "status": status})
+                start = boundary_ts
+
+        timerange_status_all.append({"start": start, "end": end, "status": status})
+    timerange_status_all.sort(key=lambda x: x["start"])
+    print(f"all: {timerange_status_all}")
+
+    # 重なっている時間帯のうち、優先度の高い予定を抽出
+    timerange_status = []
+
+    prev_start = timerange_status_all[0]["start"]
+    prev_end = timerange_status_all[0]["end"]
+    prev_status = timerange_status_all[0]["status"]
+
+    for i in range(len(timerange_status_all) - 1):
+        next_start = timerange_status_all[i + 1]["start"]
+        next_end = timerange_status_all[i + 1]["end"]
+        next_status = timerange_status_all[i + 1]["status"]
+
+        if prev_end <= next_start:
+            # 重なっていない
+            timerange_status.append({"start": prev_start, "end": prev_end, "status": prev_status})
+
+            if i == len(timerange_status_all) - 2:
+                # 最後のnextを追加して終了
+                timerange_status.append({"start": next_start, "end": next_end, "status": next_status})
+                break
+            else:
+                prev_start = next_start
+                prev_end = next_end
+                prev_status = next_status
+
+        else:
+            # 重なっている場合、必ず時間帯が同じ
+            assert prev_start == next_start
+            assert prev_end == next_end
+            status_prior = compare_status(prev_status, next_status)
+
+            if i == len(timerange_status_all) - 2:
+                # ラスト
+                timerange_status.append({"start": prev_start, "end": prev_end, "status": status_prior})
+            else:
+                prev_status = status_prior
+
+    timerange_status.sort(key=lambda x: x["start"])
+
+    print(f"timerange: {timerange_status}")
+
+    # 連続する時間帯をマージ
+    merged_status = []
+
+    prev_start = timerange_status[0]["start"]
+    prev_end = timerange_status[0]["end"]
+    prev_status = timerange_status[0]["status"]
+
+    for i in range(len(timerange_status) - 1):
+        next_start = timerange_status[i + 1]["start"]
+        next_end = timerange_status[i + 1]["end"]
+        next_status = timerange_status[i + 1]["status"]
+
+        if prev_end < next_start or prev_status != next_status:
+            # prevは確定
+            if prev_status != -1:  # 予定なしは追加しない
+                merged_status.append(
+                    {
+                        "start": prev_start.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                        "end": prev_end.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                        "status": prev_status,
+                    }
+                )
+
+            if i < len(timerange_status) - 2:
+                # 次 (next) に進む
+                prev_start = next_start
+                prev_end = next_end
+                prev_status = next_status
+            else:
+                # 最後のnextを追加して終了
+                if next_status != -1:
+                    merged_status.append(
+                        {
+                            "start": next_start.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                            "end": next_end.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                            "status": next_status,
+                        }
+                    )
+                break
+
+        else:
+            # prevとnextをマージ
+            # start: prev_startのまま
+            # end: next_endに更新
+            prev_end = next_end
+            # status: prev_statusのまま
+
+    print(f"status: {merged_status}")
+
+    # Eventの形式に変換
+    merged_events = [
+        {
+            "summary": busy_status[event["status"]],
+            "start": event["start"],
+            "end": event["end"],
+            "isAllDay": False,
+        }
+        for event in merged_status
+    ]
+
+    return merged_events
+
+
+def compare_status(status1, status2):
+    """2つのBusyStatusを比較し、優先度の高い方を返す"""
+    # ↑ 最低優先度
+    # -1: "予定なし"
+    # 0 : "空き時間",
+    # 4 : "他の場所",
+    # 1 : "仮の予定",
+    # 2 : "予定あり",
+    # 3 : "不在",
+    # ↓ 最高優先度
+
+    if status1 == 3 or status2 == 3:
+        return 3
+    elif status1 == 2 or status2 == 2:
+        return 2
+    elif status1 == 1 or status2 == 1:
+        return 1
+    elif status1 == 4 or status2 == 4:
+        return 4
+    elif status1 == 0 or status2 == 0:
+        return 0
+    else:
+        return -1
 
 
 if __name__ == "__main__":
